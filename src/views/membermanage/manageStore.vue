da8bef5d519e6a3a646c8349c16bc95acf070047
lizarku <lizarku8@gmail.com>
2024-01-22 02:29:53 +0900

    url

--- src/views/membermanage/manageStore.vue
+++ src/views/membermanage/manageStore.vue
@@ -73,8 +73,7 @@ export default {
         const state = reactive({
             storeList: [],
             regStore: {},
-            baseUrl: 'http://13.209.35.219/',
-            // window.location.origin,
+            baseUrl: window.location.origin,
             googQr: 'https://chart.apis.google.com/chart?cht=qr&chs=250x250&chl='
         });
         onMounted(() => {
