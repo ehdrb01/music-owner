d89f4bdf496d5a56e42a022e59907c97813f64b6
KBHCR-이충건\DG371 <lizarku8@gmail.com>
2024-01-25 11:23:08 +0900

    list 수정

--- src/views/playlist/list.vue
+++ src/views/playlist/list.vue
@@ -1,69 +1,73 @@
 <template>
-  <div id="list">
-    <div v-if="state.store">
-      <!-- 리스트 카테고리 -->
-      <div class="cate">
-        <div class="inner t_center inner1104" style="margin:0 auto;">
-          <div class="title fs35 fwt600">
-            <div>
-              <ul class="lst_area">
-                <li>
-                  <!-- <div class="tit fs17 fwt500">상호명</div> -->
-                  <input type="text" class="wrt_ipt readonly store_name" :value="state.store.storeNm + '의 플레이리스트('+state.nowTimer+')'"
-                    readonly="readonly">
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- 리스트 컨텐츠 -->
-      <div class="content bc8 " id="playlist">
-        <div class="inner inner1104 w100 m_center">
-          <!-- 공통 리스트 디자인 -->
-          <div id="comp_lst_area">
-            <ul class="comp_lst flex fxwrap" v-if="state.musiclist.length > 0">
-              <!-- 리스트 each-->
-              <li v-for="(item, index) in  state.musiclist "
-                :class="state.storeInfo?.adminYn && item.playingYn == 'Y' ? 'h350' : 'h40'" :key="index" class=" playing">
-                <div class="music_list" :class="item.playingYn == 'Y' ? 'glow' : ''">
-                  <div class="icon" :class="item.playingYn == 'Y' ? 'green' : 'red'">
-                    <i class="bi bi-headphones"></i>
-                  </div>
-                  <div class="music_info">
-                    <div class="singer">{{ item.reqSingerNm }}</div>
-                    <div class="float">-</div>
-                    <div class="song">{{ item.reqSongNm }}</div>
-                  </div>
-                  <div class="type" :class="item.playingYn == 'Y' ? 'green' : ''"> {{
-                    item.playingYn == 'Y' ? '재생중' : '일반'
-                  }}
-                  </div>
-                  <!-- @click="playMusic(item)" -->
-                  <!-- <div v-if="state.storeInfo?.adminYn == true" class="play_icon"><i class="bi bi-youtube"></i></div> -->
-                  <!-- @click="nextMusic(item)" -->
-                  <!-- <div v-if="state.storeInfo?.adminYn == true" class="type">완료하기</div> -->
+    <div id="list">
+        <div v-if="state.store">
+            <!-- 리스트 카테고리 -->
+            <div class="cate">
+                <div class="inner t_center inner1104" style="margin:0 auto;">
+                    <div class="title fs35 fwt600">
+                        <div>
+                            <ul class="lst_area">
+                                <li>
+                                    <!-- <div class="tit fs17 fwt500">상호명</div> -->
+                                    <input type="text" class="wrt_ipt readonly store_name"
+                                        :value="state.store.storeNm + '의 플레이리스트(' + state.nowTimer + ')'"
+                                        readonly="readonly">
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-                <div class="player" :class="state.storeInfo?.adminYn && item.playingYn == 'Y' ? 'h300' : 'h0'"
-                  v-if="state.storeInfo?.adminYn && item.playingYn == 'Y'">
-                  <iframe width="100%" height="300" :src="item.musicUrl" title="YouTube video player" frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowfullscreen></iframe>
-                </div>
-              </li>
-              <!-- 리스트 !each-->
-            </ul>
-            <div class="not_playlist" v-else-if="state.musiclist.length == 0">
-              <i class="bi bi-chat-dots"></i>신청곡이 없습니다.
             </div>
-          </div>
+            <!-- 리스트 컨텐츠 -->
+            <div class="content bc8 " id="playlist">
+                <div class="inner inner1104 w100 m_center">
+                    <!-- 공통 리스트 디자인 -->
+                    <div id="comp_lst_area">
+                        <ul class="comp_lst flex fxwrap" v-if="state.musiclist.length > 0">
+                            <!-- 리스트 each-->
+                            <li v-for="(item, index) in  state.musiclist "
+                                :class="state.storeInfo?.adminYn && item.playingYn == 'Y' ? 'h350' : 'h40'" :key="index"
+                                class=" playing">
+                                <div class="music_list" :class="item.playingYn == 'Y' ? 'glow' : ''">
+                                    <div class="icon" :class="item.playingYn == 'Y' ? 'green' : 'red'">
+                                        <i class="bi bi-headphones"></i>
+                                    </div>
+                                    <div class="music_info">
+                                        <div class="singer">{{ item.reqSingerNm }}</div>
+                                        <div class="float">-</div>
+                                        <div class="song">{{ item.reqSongNm }}</div>
+                                    </div>
+                                    <div class="type" :class="item.playingYn == 'Y' ? 'green' : ''"> {{
+                                        item.playingYn == 'Y' ? '재생중' : '일반'
+                                    }}
+                                    </div>
+                                    <!-- @click="playMusic(item)" -->
+                                    <!-- <div v-if="state.storeInfo?.adminYn == true" class="play_icon"><i class="bi bi-youtube"></i></div> -->
+                                    <!-- @click="nextMusic(item)" -->
+                                    <!-- <div v-if="state.storeInfo?.adminYn == true" class="type">완료하기</div> -->
+                                </div>
+                                <div class="player"
+                                    :class="state.storeInfo?.adminYn && item.playingYn == 'Y' ? 'h300' : 'h0'"
+                                    v-if="state.storeInfo?.adminYn && item.playingYn == 'Y'">
+                                    <iframe width="100%" height="300" :src="item.musicUrl" title="YouTube video player"
+                                        frameborder="0"
+                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                        allowfullscreen></iframe>
+                                </div>
+                            </li>
+                            <!-- 리스트 !each-->
+                        </ul>
+                        <div class="not_playlist" v-else-if="state.musiclist.length == 0">
+                            <i class="bi bi-chat-dots"></i>신청곡이 없습니다.
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
+        <div class="not_playlist" v-if="!state.store">
+            <i class="bi bi-chat-dots"></i>존재하지 않는 스토어 입니다.
+        </div>
     </div>
-    <div class="not_playlist" v-if="!state.store">
-      <i class="bi bi-chat-dots"></i>존재하지 않는 스토어 입니다.
-    </div>
-  </div>
 </template>
 <script>
 import { onMounted, reactive, watch } from 'vue';
@@ -85,7 +89,8 @@ export default {
             lastPLayed: 0,
             lastCalled: 0,
             minute: 0,
-            seconde: 0
+            seconde: 0,
+            timer: 4 * 60 * 1000
         });
         watch(state.insideList, (newValue, oldValue) => {
             console.log('insideList 변경' + newValue + '/' + oldValue);
@@ -95,26 +100,29 @@ export default {
             deep: true
         });
         onMounted(async () => {
-            state.storeInfo = JSON.parse(localStorage.getItem('userInfo'));
-            if (state.storeInfo?.storeNo == route.params.storeNo
-        && state.storeInfo?.userType == 'store') {
-                state.storeInfo.adminYn = true;
-            }
             getStore();
             lastPLayed();
-            goMusic();
-            setInterval(() => {
-                getReadyMusic();
-            }, 5000);
-            setInterval(() => {
+            state.storeInfo = JSON.parse(localStorage.getItem('userInfo'));
+            if (state.storeInfo?.storeNo == route.params.storeNo
+                && state.storeInfo?.userType == 'store') {
+                state.storeInfo.adminYn = true;
                 goMusic();
-            },  4 * 60 * 1000);
+                setInterval(() => {
+                    getReadyMusic();
+                }, 5000);
+                setInterval(() => {
+                    goMusic();
+                }, timer);
+                setInterval(() => {
+                    state.nowTimer++;
+                }, 1000, true);
+            } else {
+                setInterval(() => {
+                    getMusicList();
+                }, 5 * 1000);
+            }
 
-            setInterval(() => {
-                state.nowTimer++;
-            }, 1000, true);
-      
-            
+
         });
         const playedMusic = async () => {
             const param = {
@@ -126,7 +134,7 @@ export default {
             try {
                 if (response.data.code === 200) {
                     console.log('playedMusic >>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
-              // getMusicList();
+                    // getMusicList();
                 } else {
                     console.log(response.data.message);
                 }
@@ -146,7 +154,7 @@ export default {
                     getMusicList();
                     console.log('playingMusic>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
                     console.log(param.reqSongNo);
-      
+
                 } else {
                     console.log(response.data.message);
                 }
@@ -166,15 +174,6 @@ export default {
                     console.log('getMusicList>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
                     console.log(response);
                     state.musiclist = response.data.data.list;
-                    state.nowTimer = 0;
-                    // if (response.data.data.list.length > 0) {
-                        // console.log('신청곡 재생');
-                        // await playingMusic(response.data.data.list[0]);
-                    // }
-                    //  else {
-                    //     console.log('신청곡 대기중');
-                    //     await waitList();
-                    // }
                 } else {
                     console.log(response.data.message);
                 }
@@ -197,7 +196,7 @@ export default {
                 console.log(e);
             }
         };
-        const initMusicList =  async () => {
+        const initMusicList = async () => {
             const param = {
                 reqStoreNo: route.params.storeNo
             };
@@ -214,10 +213,10 @@ export default {
             } catch (e) {
                 console.log(e);
             }
-          
+
         };
 
-        const getReadyMusic =  async () => {
+        const getReadyMusic = async () => {
             console.log('곡 수집중');
             const param = {
                 reqStoreNo: route.params.storeNo
@@ -252,13 +251,16 @@ export default {
             } catch (e) {
                 console.log(e);
             }
-            
+
         };
         const goMusic = () => {
             getMusicList().then(() => {
                 if (state.musiclist.length > 0) {
                     playingMusic().then(() => {
-                        playedMusic();
+                        state.nowTimer = 0;
+                        setTimeout(() => {
+                            playedMusic();
+                        }, timer - (10 * 1000));
                     });
                 }
             });
